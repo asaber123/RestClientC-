6eea2fe0eea48c8e0b84facdfe136f2389950076
Åsa Berglund <you@example.com>
2022-01-11 09:47:56 +0100

    Initial check-in of module RestCSharp

---
+++ RestCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
